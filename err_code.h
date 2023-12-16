1c6b6aad412fd7148d8fd8a664b19f8483637093
phonght32 <thanhphongho1998@gmail.com>
2023-12-16 11:13:16 +0700

    update error code for stm32

--- err_code.h
+++ err_code.h
@@ -5,10 +5,10 @@
 extern "C" {
 #endif
 
-#include "esp_err.h"
+#include "stm32f4xx.h"
 
-#define ERR_CODE_SUCCESS 	ESP_OK
-#define ERR_CODE_FAIL 		ESP_FAIL
+#define ERR_CODE_SUCCESS 	HAL_OK
+#define ERR_CODE_FAIL 		HAL_ERROR
 #define ERR_CODE_NULL_PTR 	(uint32_t)0xFFFE
 
 typedef uint32_t err_code_t;
