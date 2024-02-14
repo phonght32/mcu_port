52a76bfad1b90e0a149ce65d8f1390fd30a191c8
phonght32 <thanhphongho1998@gmail.com>
2024-02-14 18:32:30 +0700

    add stm32l4 error code

--- err_code.h
+++ err_code.h
@@ -21,6 +21,14 @@ extern "C" {
 #define ERR_CODE_NULL_PTR 	(uint32_t)0xFFFE
 #endif
 
+#ifdef STM32L4R5xx
+#include "stm32l4xx.h"
+
+#define ERR_CODE_SUCCESS 	HAL_OK
+#define ERR_CODE_FAIL 		HAL_ERROR
+#define ERR_CODE_NULL_PTR 	(uint32_t)0xFFFE
+#endif
+
 typedef uint32_t err_code_t;
 
 #ifdef __cplusplus
